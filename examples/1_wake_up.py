35b42ae26181ea390b72140b61c9d5070ee88e29
Lovmong <1967471283@qq.com>
2022-12-22 15:47:06 +0800

    optimize

--- examples/1_wake_up.py
+++ examples/1_wake_up.py
@@ -4,31 +4,33 @@ from time import sleep
 from preset_actions import pant
 from preset_actions import body_twisting
 
-my_dog = Pidog()
-sleep(0.5)
-
+my_dog = Pidog(head_init_angles=[0, 0, -30])
+sleep(1)
 
 def wake_up():
+    # stretch
     my_dog.rgb_strip.set_mode(
         'breath', color='yellow', brightness=0.8, delay=0.095)
+    my_dog.do_action('stretch', speed=30)
     my_dog.head_move([[0, 0, 30]]*2, immediately=True)
-    my_dog.wait_all_done()
-    my_dog.do_action('stretch', speed=20)
     my_dog.wait_all_done()
     sleep(0.2)
     body_twisting(my_dog)
     my_dog.wait_all_done()
-    sleep(0.2)
+    sleep(0.5)
     my_dog.head_move([[0, 0, -30]], immediately=True, speed=90)
+    # sit and wag_tail
     my_dog.do_action('sit', speed=50)
     my_dog.wait_legs_done()
     my_dog.do_action('wag_tail', step_count=10, speed=100)
-    my_dog.rgb_strip.set_mode('breath', color=[
-                              245, 10, 10], brightness=0.8, delay=0.002)
+    my_dog.rgb_strip.set_mode('breath', color=[245, 10, 10], brightness=0.8, delay=0.002)
     pant(my_dog, pitch_comp=-30)
     my_dog.wait_all_done()
-    my_dog.rgb_strip.close()
-
+    # hold
+    my_dog.do_action('wag_tail', step_count=1, speed=30)
+    my_dog.rgb_strip.set_mode('breath', 'pink', delay=0.14)
+    while True:
+        sleep(1)
 
 if __name__ == "__main__":
     try:
