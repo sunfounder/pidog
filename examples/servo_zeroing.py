68552211acbeadb406c4d35dc79e3ec44ec76f6f
Lovmong <1967471283@qq.com>
2023-03-16 17:20:54 +0800

    match robot-hat v2.0

--- examples/servo_zeroing.py
+++ examples/servo_zeroing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from robot_hat import PWM, Servo
+from robot_hat import Servo
 from robot_hat.utils import reset_mcu
 from time import sleep
 
@@ -10,9 +10,9 @@ if __name__ == '__main__':
     servos = []
     for i in range(12):
         print(f"Servo {i} set to zero")
-        Servo(PWM(i)).angle(10)
+        Servo(i).angle(10)
         sleep(0.1)
-        Servo(PWM(i)).angle(0)
+        Servo(i).angle(0)
         sleep(0.1)
     while True:
         sleep(1)
