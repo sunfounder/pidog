90f149292c3c0ba69b6f35f205e60546ffadb730
Lovmong <1967471283@qq.com>
2024-02-23 15:37:22 +0800

    change polarity, optimize slide

--- pidog/dual_touch.py
+++ pidog/dual_touch.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python3
 from robot_hat import Pin
-from time import sleep
+import time
 
 
 class DualTouch():
+
+    SLIDE_MAX_INTERVAL = 0.5 # second, Maximum effective interval for sliding detection
+
     def __init__(self, sw1='D2', sw2='D3'):
 
-        self.touch_1 = Pin(sw1)
-        self.touch_2 = Pin(sw2)
+        self.touch_L = Pin(sw1)
+        self.touch_R = Pin(sw2)
+        self.last_touch = 'N'
+        self.last_touch_time = 0
+
+    # def read(self):
+    #     if self.touch_L.value() == 1:
+    #         time.sleep(0.1)
+    #         if self.touch_R.value() == 1:
+    #             return 'LS'
+    #         else:
+    #             return 'L'
+    #     elif self.touch_R.value() == 1:
+    #         time.sleep(0.1)
+    #         if self.touch_L.value() == 1:
+    #             return 'RS'
+    #         else:
+    #             return 'R'
+    #     return 'N'
 
     def read(self):
-        if self.touch_1.value() == 0:
-            sleep(0.1)
-            if self.touch_2.value() == 0:
-                return 'LS'
+        if self.touch_L.value() == 1:
+            if self.last_touch == 'R' and\
+                time.time() - self.last_touch_time <= self.SLIDE_MAX_INTERVAL:
+                val = 'RS'
             else:
-                return 'L'
-        elif self.touch_2.value() == 0:
-            sleep(0.1)
-            if self.touch_1.value() == 0:
-                return 'RS'
+                val = 'L'
+            self.last_touch_time = time.time()
+            self.last_touch = 'L'
+            return val
+        elif self.touch_R.value() == 1:
+            if self.last_touch == 'L' and\
+                time.time() - self.last_touch_time <= self.SLIDE_MAX_INTERVAL:
+                val = 'LS'
             else:
-                return 'R'
+                val = 'R'
+            self.last_touch_time = time.time()
+            self.last_touch = 'R'
+            return val
         return 'N'
--- test/dual_touch_test.py
+++ test/dual_touch_test.py
@@ -5,5 +5,5 @@ touch = DualTouch('D2', 'D3')
 
 while True:
     print(
-        f"\rLeft value: {touch.touch_1.value()} | Right value: {touch.touch_2.value()}", end="          ", flush=True)
+        f"\rLeft value: {touch.touch_L.value()} | Right value: {touch.touch_R.value()} | {touch.read()}", end="          ", flush=True)
     sleep(0.05)
