53e6115245a16855dab5808e8984d53170dea9cc
haru$ <1736312423@qq.com>
2024-08-29 14:09:28 +0800

    change facebook link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,7 +117,7 @@ rst_epilog += """
 
 .. |link_sf_facebook| raw:: html
 
-    <a href="https://bit.ly/4arqbqG" target="_blank">hier</a>
+    <a href="https://bit.ly/raphaelkit" target="_blank">hier</a>
 
 .. |link_code_10_balance| raw:: html
 
