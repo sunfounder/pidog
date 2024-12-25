cada160576188dc46e64586abf654913ab1d2bcf
harumiume <127194455+harumiume@users.noreply.github.com>
2024-12-25 16:04:18 +0800

    fix language bugs.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,7 +114,7 @@ rst_epilog += """
 
 .. |link_voice_options| raw:: html
 
-    <a href="https://platform.openai.com/docs/guides/text-to-speech/voice-options" target="_blank">Voice options</a>
+    <a href="https://platform.openai.com/docs/guides/text-to-speech/voice-options" target="_blank">Opzioni vocali</a>
 
 .. |link_iso_language_code| raw:: html
 
@@ -123,7 +123,7 @@ rst_epilog += """
 
 .. |link_microphone| raw:: html
 
-    <a href="https://www.sunfounder.com/products/mini-usb-microphone?_pos=2&_sid=d05c80026&_ss=r" target="_blank">Microphone link</a>
+    <a href="https://www.sunfounder.com/products/mini-usb-microphone?_pos=2&_sid=d05c80026&_ss=r" target="_blank">Collegamento del microfono</a>
 
 .. |link_openai_platform| raw:: html
 
@@ -131,7 +131,7 @@ rst_epilog += """
 
 .. |link_sf_facebook| raw:: html
 
-    <a href="https://bit.ly/raphaelkit" target="_blank">here</a>
+    <a href="https://bit.ly/raphaelkit" target="_blank">qui</a>
 
 .. |link_code_10_balance| raw:: html
 
@@ -171,7 +171,7 @@ rst_epilog += """
 
 .. |link_PiDog| raw:: html
 
-    <a href="https://www.sunfounder.com/products/sunfounder-pidog-robot-dog-kit-for-raspberry-pi?_pos=1&_sid=313a4c894&_ss=r&variant=44517213896939" target="_blank">Purchase Link for PiDog</a>
+    <a href="https://www.sunfounder.com/products/sunfounder-pidog-robot-dog-kit-for-raspberry-pi?_pos=1&_sid=313a4c894&_ss=r&variant=44517213896939" target="_blank">Link per l'acquisto di PiDog</a>
 
 .. |link_Pi_Dog| raw:: html
 
