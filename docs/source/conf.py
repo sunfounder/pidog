7342017b1f5c0da4e4b7e90012c9a14f56fb2ad2
harumiume <127194455+harumiume@users.noreply.github.com>
2025-08-08 15:22:51 +0800

    update_robot_hat_V4_link.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,7 +142,7 @@ rst_epilog += """
 
 .. |link_robot_hat_v4| raw:: html
 
-    <a href="https://docs.sunfounder.com/projects/robot-hat-v4/en/latest/hardware_introduction.html" target="_blank">Robot HAT</a>
+    <a href="https://docs.sunfounder.com/projects/robot-hat-v4/en/latest/robot_hat_v4/hardware_introduction.html" target="_blank">Robot HAT</a>
 
 .. |link_robot_hat_v5| raw:: html
 
