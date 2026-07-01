900c54f95cb9b001eb602a2ea632458a17a68ae3
Cavon <cavonxx@gmail.com>
2026-07-01 10:20:18 +0800

    feat: voice_assistant 添加 EdgeTTS 使用示例注释

--- pidog/voice_assistant.py
+++ pidog/voice_assistant.py
@@ -1,1 +1,4 @@
 from robot_hat.voice_assistant import VoiceAssistant
+# To use EdgeTTS instead of default Piper TTS:
+#   from sunfounder_voice_assistant.tts import EdgeTTS
+#   va = VoiceAssistant(llm, tts=EdgeTTS(voice="en-US-AriaNeural"))
